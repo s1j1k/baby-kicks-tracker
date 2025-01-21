5c59714d8e9efe6661470c8fffe13dbfeb5a3ec3
Sally Kalumba <sallykalumba@gmail.com>
2025-01-21 15:36:47 +1100

    add 3hr timestamps

--- components/timeline.vue
+++ components/timeline.vue
@@ -1,47 +1,51 @@
 <template>
-  <!-- TIMELINE LINE -->
-  <div class="relative">
-    <!-- TODO ensure the timeline is broken clearly into 24 hours intervals -->
-    <div class="bg-blue-400 rounded-lg h-3 mb-10"></div>
-    <!-- Add data points -->
-    <!-- TODO use a v-for -->
-    <!-- TODO adjust the size/number of icons in a cluster based on the provided metadata (if any) -->
+    <!-- TIMELINE -->
+    <div class="relative w-full mb-12">
+      <!-- Horizontal Timeline Bar -->
+      <div class="bg-blue-400 rounded-lg h-3 w-full relative"></div>
+  
+      <!-- Timestamps -->
+      <div class="relative">
+        <div 
+          v-for="(time, index) in timestamps" 
+          :key="index" 
+          :style="{ left: `${(index / (timestamps.length - 1)) * 100}%` }"
+          class="absolute top-1 -translate-x-1/2 text-gray-500 text-xs"
+        >
+          {{ time }}
+        </div>
+      </div>
 
-    <!-- TODO add timestamps for today -->
-
-    <!-- TODO allow scrolling -->
-
-    <!-- TODO add current time -->
-
-    <!-- TODO make the timeline a circle? -->
-    <!-- TODO look at other apps -->
-    <!-- TODO use a library forr the timeline? -->
-
-    <!-- TIMELINE icons -->
-    <div class="absolute top-0 left-0 -translate-y-1/4">
+      <!-- Foot Icons -->
+    <div 
+      v-for="(kick, index) in kicks" 
+      :key="index" 
+      :style="{ left: `${kick.position}%` }"
+      class="absolute top-0 -translate-y-1/2"
+    >
       <UIcon name="mingcute:foot-fill" class="w-7 h-7 text-blue-950" />
     </div>
-
-    <!-- TODO add times below -->
-    <!-- NOTE can we just use relative to timeline to establish proper spacing? -->
-    <div v-for="time in timestamps">
-      <div class="absolute top-0 left-0 -translate-y-1/4">
-        <div class="text-gray-500 text-sm">{{ time }}</div>
-      </div>
     </div>
-  </div>
-</template>
+  </template>
+  
+  <script setup lang="ts">
+  const timestamps = [
+    "12 AM",
+    // "3 AM",
+    "6 AM",
+    // "9 AM",
+    "12 PM",
+    // "3 PM",
+    "6 PM",
+    // "9 PM",
+    "12 AM",
+  ];
 
-<script setup lang="ts">
-const timestamps = [
-  "12 AM",
-  "3 AM",
-  "6 AM",
-  "9 AM",
-  "12 PM",
-  "3 PM",
-  "6 PM",
-  "9 PM",
-  "12 AM",
+  // TODO take kicks data as a param
+  // Example kicks data (positions are percentages along the timeline)
+const kicks = [
+  { time: "7:15 AM", position: 30 }, // Example: 30% along the timeline
+  { time: "2:45 PM", position: 70 },
 ];
-</script>
+  </script>
+  
