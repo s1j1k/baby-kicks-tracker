35b7d9729f0ca56354738ca9b621fd93afbe28e7
Sally Kalumba <sallykalumba@gmail.com>
2025-01-23 13:01:23 +1100

    rename config to camelcase

--- plugins/firebase.client.ts
+++ plugins/firebase.client.ts
@@ -14,13 +14,13 @@ export default defineNuxtPlugin((nuxtApp) => {
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
   const firebaseConfig = {
-    apiKey: config.FB_API_KEY,
-    authDomain: config.FB_AUTH_DOMAIN,
-    projectId: config.FB_PROJECT_ID,
-    storageBucket: config.FB_STORAGE_BUCKET,
-    messagingSenderId: config.FB_MESSAGING_SENDER_ID,
-    appId: config.FB_APP_ID,
-    measurementId: config.FB_MEASUREMENT_ID,
+    apiKey: config.fbApiKey,
+    authDomain: config.fbAuthDomain,
+    projectId: config.fbProjectId,
+    storageBucket: config.fbStorageBucket,
+    messagingSenderId: config.fbMessagingSenderId,
+    appId: config.fbAppId,
+    measurementId: config.fbMeasurementId,
   };
 
   // Initialize Firebase
